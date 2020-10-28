6c5ff6ce6eca1d21dc4300a30164ffb55ecc6b36
Sonal <singh0809sonal@gmail.com>
2020-10-29 00:14:35 +0530

    Quick Short

---
+++ Week-7/Quicksort.js
@@ -0,0 +1,45 @@
+function swap(items, leftIndex, rightIndex){
+    var temp = items[leftIndex];
+    items[leftIndex] = items[rightIndex];
+    items[rightIndex] = temp;
+}
+
+function partition(items, left, right) {
+    // [10, 20, 80, 50, 70, 30, 40, 60, 90];
+    var pivot   = items[Math.floor((right + left) / 2)], //middle element
+        i       = left, //left pointer
+        j       = right; //right pointer
+    while (i <= j) {
+        // console.log("i", i,"j",j,"pivot",pivot);
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items, i, j); //swap two elements
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
+function quickSort(items, left, right) {
+    var index;
+    if (items.length > 1) {
+        index = partition(items, left, right); //index returned from partition
+        if (left < index - 1) { //more elements on the left side of the pivot
+            quickSort(items, left, index - 1);
+        }
+        if (index < right) { //more elements on the right side of the pivot
+            quickSort(items, index, right);
+        }
+    }
+    return items;
+}
+
+let arr = [10, 20, 80, 50, 90, 30, 40, 60, 70];
+let result = quickSort(arr, 0, arr.length-1);
+console.log(result);
