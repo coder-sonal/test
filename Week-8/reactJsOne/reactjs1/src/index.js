86b0e22131b5260b85334dad3efa14a864de911c
Sonal <singh0809sonal@gmail.com>
2021-01-06 14:29:22 +0530

    reactJsOne

---
+++ Week-8/reactJsOne/reactjs1/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Create a React Compoenent
+const App = () =>{
+    return <h1>Hello world</h1>;
+};
+
+//Render it to the DOM
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+);
